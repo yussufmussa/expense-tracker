5bd0dd92ea388e4030e94459f02eb50aebd6c853
yussufmussa <alphillipsa@gmail.com>
2023-11-09 23:49:58 +0300

    change from optional to composition api

--- src/App.vue
+++ src/App.vue
@@ -4,30 +4,26 @@
   <div class="container">
     <Balance />
     <IncomeExpenses />
-    <TranscationList />
+    <TranscationList :transactions="transactions" />
     <AddTransaction />
     
   </div>
 </template>
 
 
-<script>
+<script setup>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpenses from './components/IncomeExpenses.vue';
 import TranscationList from './components/TranscationList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
+import { ref } from 'vue';
+const transactions = ref([
+    { id: 1, text: 'Flower', amount: -19.99 },
+    { id: 2, text: 'Salary', amount: 299.99 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camer', amount: 150 },
 
-
-
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TranscationList,
-    AddTransaction,
-  }
-}
+]);
 </script>
