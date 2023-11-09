1d3797dd404ecbab09c078c9eb20a008a0e9da66
yussufmussa <alphillipsa@gmail.com>
2023-11-09 23:37:30 +0300

    setting up project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <Header />
+
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TranscationList />
+    <AddTransaction />
+    
+  </div>
+</template>
+
+
+<script>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TranscationList from './components/TranscationList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+
+
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpenses,
+    TranscationList,
+    AddTransaction,
+  }
+}
+</script>
---
+++ src/components/TranscationList.vue
@@ -0,0 +1,25 @@
+<template>
+     <h3>History</h3>
+      <ul id="list" class="list">
+        <li  v-for="transaction in transactions " :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus' ">
+          {{ transaction.text }}<span>${{ transaction.amount }}</span><button class="delete-btn">x</button>
+        </li>
+        
+      </ul>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            transactions: [
+                {id: 1, text: 'Flower', amount: -19.99},
+                {id: 2, text: 'Salary', amount: 299.99},
+                {id: 3, text: 'Book', amount: -10},
+                {id: 4, text: 'Camer', amount: 150},
+
+            ],
+        };
+    },
+};
+</script>
